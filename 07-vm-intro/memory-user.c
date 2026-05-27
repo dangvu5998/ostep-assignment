1ff9f466c1936f0de2794592e77df8bf1fb77daf
Vu <dangvu5998@gmail.com>
2026-05-27 10:30:50 +0700

    add memory-user file

---
+++ 07-vm-intro/memory-user.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s <memory in MB> <time in seconds>", argv[0]);
+        return 1;
+    }
+    int memory_mb = atoi(argv[1]);
+    int time_in_s = atoi(argv[2]);
+    size_t total_bytes = (size_t)memory_mb * 1048576;
+    char* hold_memory = malloc(memory_mb * 1048576);
+    if (hold_memory == NULL) {
+        printf("Failed to allocate memory");
+        return 1;
+    }
+    time_t start_time = time(NULL);
+    while (time(NULL) - start_time < time_in_s) {
+        for (int i = 0; i < memory_mb * 1048576; i++) {
+            *(hold_memory + i) = 'a';
+        }
+    }
+
+    free(hold_memory);
+    return 0;
+}
